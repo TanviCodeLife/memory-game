35348a6a010215d64f3b8260e8b3978719af2e37
Tanvi Garg <tanvi.garg23@gmail.com>
2019-01-03 16:35:35 -0800

    function to append divs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { MemoryGame, Card } from './memory';
 
 $(document).ready(function() {
   
--- src/memory-game.js
+++ src/memory-game.js
@@ -1,23 +1,27 @@
 export class MemoryGame {
   constructor() {
     this.No_Of_Squares = 10;
-    this.cards = [];
+    this.randomCards = [];
     this.clicks = 0;
     this.firstCard = "";
     this.secondCard = "";
+    this.buildCards();
     this.buildCardGrid();
-    this.buildBoard();
   }
-    
+
+  //build random cards in a grid and push them into a random Cards array
   buildCardGrid() {
     for(let i = 0; i <= this.No_Of_Squares; i++){
-      let card = new Card(i);
-      this.cards.push(card); 
+      let randomNumber = Math.floor(Math.random() * this.No_Of_Squares);
+      let card = new Card(randomNumber);
+      this.randomCards.push(card);
+      $("#board").append("<div class='grid'></div>");
     }
   }
-
 }
-
+    
 export class Card{
-
+  constructor(CardId) {
+    this.id = CardId;
+  }
 }
