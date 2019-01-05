ab2c543c5b119c74a19ed5059b7a9aa9ccbea4e4
Tanvi Garg <tanvi.garg23@gmail.com>
2019-01-05 00:13:02 -0800

    attach event listeners to all divs and get their data attribute value

--- src/main.js
+++ src/main.js
@@ -2,22 +2,21 @@ import { MemoryGame } from './memory-game';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import './styles.css';import { tmpdir } from 'os';
 
-
+var game = new MemoryGame();
 
 function shuffle() {
   $(".memory-game").each(function () {
     var divs = $(this).find('div');
-
     for (var i = 0; i < divs.length; i++) {
       $(divs[i]).remove();
     }
     // eslint-disable-next-line no-redeclare
     var i = divs.length;
     if (i == 0)
-      return false;
-    while (--i) {
+    return false;
+    while (i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var tempi = divs[i];
       var tempj = divs[j];
@@ -27,12 +26,30 @@ function shuffle() {
     // eslint-disable-next-line no-redeclare
     for (var i = 0; i < divs.length; i++) {
       $(divs[i]).appendTo(this);
+      game.cards.push($(divs[i]).data("value"));
     }
   });
 }
 
+
+
+  
+  // function flipCard() {
+  
+  // }
+
+function attachContactListeners() {
+  $(".memory-game").on("click", "div", function () {
+    console.log("The value of this <div> is " + $(this).data("value") + ".");
+  });
+  //flipcard1 
+  //flipcard2
+  //check match
+  //match == true; keep cards open
+  //match == false; close cards 
+}
+
 $(document).ready(function() {
+  attachContactListeners(); 
   shuffle();
 });
-
-
--- src/memory-game.js
+++ src/memory-game.js
@@ -1,34 +1,13 @@
 export function MemoryGame() {
-  this.noOfCards = 2;
-  // this.firstFlip = false;
-  // this.secondFlip = false;
-  // this.isMatch = true;
-  // this.noOfTurns = 0;
-  // this.cards = [];
+  this.noOfCards = 5;
+  this.cards = [];
+  this.firstFlip = false;
+  this.secondFlip = false;
+  this.isMatch = true;
+  this.noOfMoves = 0;
 }
 
-// function shuffle() {
-//   $(".memory-game").each(function () {
-//     let divs = $(this).find('div');
 
-//     for (let i = 0; i < divs.length; i++){  
-//       $(divs[i]).remove();
-//     }
-//     let i = divs.length;
-//     if (i == 0) 
-//       return false;
-//     while (--i) {
-//       let j = Math.floor(Math.random() * (i + 1));
-//       let tempi = divs[i];
-//       let tempj = divs[j];
-//       divs[i] = tempj;
-//       divs[j] = tempi;
-//     }
-//     for (let i = 0; i < divs.length; i++) {
-//       $(divs[i]).appendTo(this);
-//     }
-//   });
-// };
 
 //Check firstFlip
 //Check secondFlip
