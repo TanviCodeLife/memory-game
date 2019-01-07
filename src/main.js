f781e6e1aae87c96d766019aa69f2277d51eb92f
Maly Phongsavanh and Tanvi Garg <tanvi@>
2019-01-07 12:16:43 -0800

    flip card, check match, unflip mismatch and count moves

--- src/main.js
+++ src/main.js
@@ -31,50 +31,76 @@ function shuffle() {
   });
 }
 
-//function flipcard() {
-  //add class to display value
-//}
+function flipcard(value) {
+    if(value.includes("yellow")){
+      $("div[data-value=" + value + "]").addClass("yellow");
+    } else if (value.includes("red")) {
+      $("div[data-value=" + value + "]").addClass("red");
+    }
+    else if (value.includes("blue")) {
+      $("div[data-value=" + value + "]").addClass("blue");
+    }
+    else if (value.includes("orange")) {
+      $("div[data-value=" + value + "]").addClass("orange");
+    }
+    else if (value.includes("pink")) {
+      $("div[data-value=" + value + "]").addClass("pink");
+    }
+}
+
+function unflipCard(value) {
+  var classname = value.match(/^1/);
+  $("div[data-value=" + value + "]").removeClass(classname);
+}
 
 
-// function checkMatch() {
-//   if (game.firstFlipValue === game.secondFlipValue) {
-//     matched();
-//   }
-// }
+function checkMatch() {
+  if (game.secondFlipValue.includes(game.firstFlipValue) || game.firstFlipValue.includes(game.secondFlipValue)) {
+    game.isMatch = true;
+    game.noOfMoves++;
+  } else {
+    game.isMatch = false;
+    game.noOfMoves++;
+  }
+  unflipCard(game.secondFlipValue);
+  unflipCard(game.secondFlipValue);
+  game.clicks = 0;
+  return console.log(game.isMatch)
+}
 
-// function matched() {
-  //use $("div[name=" + game.firstFlipValue + "]") to access the element
-  //disable click events for matched cards
-//}
-
-//function unmatched() {
-  //disable clicks more clicks until unmatch cards flipped back(hidden)
-  //add class to hide value for firstFlipValue and secondFlipValue  
-//}
 
 //function reset() {
   //reset firstFlipValue and secondFlipValue
   //reset game.clicks
-}
+// }
 
 
 function attachContactListeners() {
   $(".memory-game").on("click", "div", function () {
-    //console.log("The value of this <div> is " + $(this).data("value") + ".");
-    flipcard();
+    console.log("The value of this <div> is " + $(this).data("value") + ".");
   });
 }
 
+
 $(document).ready(function() {
   shuffle();
-  attachContactListeners(); 
-  if(game.clicks == 0) {
-    game.firstFlipValue = $(this).data("value");
-    game.clicks++;
-  } else if(game.clicks == 1) {
-    game.secondFlipValue = $(this).data("value");
-    //check match
-  }
-  //add button for reset
+  attachContactListeners();
+
+  $(".memory-card").click(function() {
+    if(game.clicks === 0) {
+      game.firstFlipValue = $(this).data("value");
+      flipcard(game.firstFlipValue);
+      $("div[data-value=" + game.firstFlipValue + "]").off("click");
+      console.log("Game Flip Val " + game.firstFlipValue);
+      console.log("Game clicks " + game.clicks);
+      game.clicks++;
+    } else if(game.clicks === 1) {
+      game.secondFlipValue = $(this).data("value");
+      flipcard(game.secondFlipValue);
+      $("div[data-value=" + game.secondFlipValue + "]").off("click");
+      checkMatch();
+    }
+    //add button for reset
   //reset game using shuffle(); and reset();
+  })
 });
--- src/memory-game.js
+++ src/memory-game.js
@@ -4,8 +4,6 @@ export function MemoryGame() {
   this.clicks = 0;
   this.firstFlipValue = "";
   this.secondFlipValue = "";
-  this.isMatch = true;
+  this.isMatch = false;
   this.noOfMoves = 0;
 }
-
-
